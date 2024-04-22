762613ac9f6b59c49011787e619d895333065648
Reika <75054515+arvanda@users.noreply.github.com>
2024-04-22 09:21:29 +0700

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anime News</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Anime News</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main id="main-content">
+        <section id="articles">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid facere doloremque labore, quidem impedit earum veritatis odio aut obcaecati explicabo quisquam omnis voluptatum accusamus maxime illum corporis aperiam! Commodi, a.
+        </section>
+        <aside id="sidebar">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque quo rem doloribus impedit, doloremque ad nihil ipsum ab! Maiores architecto obcaecati inventore iusto veritatis at voluptates aut nesciunt quasi quibusdam.
+        </aside>
+    </main>
+    <footer>
+        <p>&copy; 2024 Anime News. All rights reserved.</p>
+    </footer>
+    <script src="/js/script.js"></script>
+</body>
+</html>
